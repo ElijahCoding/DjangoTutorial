f4007bdb0ebeec84275eeff5420946b3542c1acc
LittleElijah <cuiyeqing960904@gmail.com>
2018-07-05 19:48:49 +0300

    custom registration form

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
+from accounts.forms import RegistrationForm
 
 # Create your views here.
 def home(request):
@@ -10,11 +11,11 @@ def home(request):
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('/account')
     else:
-        form = UserCreationForm()
+        form = RegistrationForm()
         args = { 'form': form }
         return render(request, 'accounts/reg_form.html', args)
