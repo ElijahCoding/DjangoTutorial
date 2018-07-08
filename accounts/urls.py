568f7106744a5c9d096c8688a19e67994e6b54b7
LittleElijah <cuiyeqing960904@gmail.com>
2018-07-08 22:29:58 +0300

    reset password

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,9 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 from . import views
-from django.contrib.auth.views import login, logout
+from django.contrib.auth.views import (
+    login, logout, password_reset, password_reset_done, password_reset_confirm,
+    password_reset_complete
+)
 
 urlpatterns = [
     url(r'^$', views.home),
@@ -9,5 +12,11 @@ urlpatterns = [
     url(r'^register/$', views.register, name='register'),
     url(r'^profile/$', views.view_profile, name='view_profile'),
     url(r'^profile/edit/$', views.edit_profile, name='edit_profile'),
-    url(r'^change-password/$', views.change_password, name='change_password')
+    url(r'^change-password/$', views.change_password, name='change_password'),
+
+    url(r'^reset-password/$', password_reset, name='reset_password'),
+    url(r'^reset-password/done/$', password_reset_done, name='reset_password_done'),
+
+    url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm, name='reset_password_confirm'),
+    url(r'^reset-password/complete/$', password_reset_complete, name='password_reset_complete')
 ]
--- tutorial/settings.py
+++ tutorial/settings.py
@@ -122,3 +122,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/account/'
+
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
